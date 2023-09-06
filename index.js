a4b87fcda9467b15dafc93307e3359b3a2439710
Fatou Binetou GAYE <gayefatimabinetou@gmail.com>
2023-09-06 12:12:27 +0200

    exercice 2

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-let a = prompt("Veuillez entrer un nombre");
-let b = prompt("Veuillez entrer un nombre");
-let button = document
-  .querySelector("#btn")
-  .addEventListener("click", function () {
-    alert(a * b);
-  });
+// Exercice 1
+// let a = prompt("Veuillez entrer un nombre");
+// let b = prompt("Veuillez entrer un nombre");
+// let button = document
+//   .querySelector("#btn")
+//   .addEventListener("click", function () {
+//     alert(a * b);
+//   });
+// Exercice 2
+let n = prompt("Saisir un nombre");
+let mod = n / 4;
+
+function moduLo() {
+  alert("le modulo de ce nombre est : " + mod);
+}
+moduLo();
