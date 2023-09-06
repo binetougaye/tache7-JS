7705e1f281eb1dc9f5ee887219194fef3af59cf9
Fatou Binetou GAYE <gayefatimabinetou@gmail.com>
2023-09-06 13:12:24 +0200

    exo 1

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Exercice 1
-// let a = prompt("Veuillez entrer un nombre");
-// let b = prompt("Veuillez entrer un nombre");
-// let button = document
-//   .querySelector("#btn")
-//   .addEventListener("click", function () {
-//     alert(a * b);
-//   });
+let a = prompt("Veuillez entrer un nombre");
+let b = prompt("Veuillez entrer un nombre");
+let button = document
+  .querySelector("#btn")
+  .addEventListener("click", function () {
+    alert(a * b);
+  });
 // Exercice 2
 let n = prompt("Saisir un nombre");
 let mod = n / 4;
